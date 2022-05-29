040ca28a8ace7e75c9322eb62d41638b855359bf
netpro123 <quanganhnguyen2382001@gmial.com>
2022-05-29 21:21:09 +0700

    add categorybar

--- movie_app_ui/lib/page/home_page.dart
+++ movie_app_ui/lib/page/home_page.dart
@@ -2,12 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:movie_app_ui/config/app_assets.dart';
 import 'package:movie_app_ui/config/app_color.dart';
 import 'package:movie_app_ui/config/text_style.dart';
+import 'package:movie_app_ui/model/movie.dart';
+import 'package:movie_app_ui/widget/home/category_bar.dart';
 import 'package:movie_app_ui/widget/home/home_header.dart';
 import 'package:movie_app_ui/widget/home/search_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -18,6 +25,8 @@ class HomePage extends StatelessWidget {
             HomeHeader(size: size),
             const SizedBox(height: 20,),
             SearchBar(size: size),
+            const SizedBox(height: 20,),
+            CategoryBar(size: size),
           ],
         ),
       ),

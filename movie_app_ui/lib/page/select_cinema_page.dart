33effb3269050f674d18ac5486efab1a3b09176a
netpro123 <quanganhnguyen2382001@gmial.com>
2022-06-02 22:11:10 +0700

    continue select cinema page

--- movie_app_ui/lib/page/select_cinema_page.dart
+++ movie_app_ui/lib/page/select_cinema_page.dart
@@ -80,25 +80,93 @@ class SelectCinema extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: 
                   days.map((e) => Builder(builder: (context){
-                    return Container(
-                      height: size.height / 8,
-                      width: size.width / 5,
-                      decoration: BoxDecoration(
-                        color: DarkTheme.darkBackground,
-                        borderRadius: BorderRadius.circular(14),
-                      ),
-                    );
+                    return buildDate(size, e);
                   },)).toList(),
               ),
             ),
             buildTitle('Central Park CGV'),
+            Container(
+              margin: EdgeInsets.only(top: 8,bottom: 16),
+              height: size.height / 15,
+              child: ListView.builder(
+                itemBuilder: (context, index) => buildTime(size, index, timeStates_1),
+                itemCount: times.length,
+                scrollDirection: Axis.horizontal,
+                ),
+              ),
             buildTitle('Kim Dong CGV'),
+            Container(
+              margin: EdgeInsets.only(top: 8,bottom: 16),
+              height: size.height / 15,
+              child: ListView.builder(
+                itemBuilder: (context, index) => buildTime(size, index, timeStates_2),
+                itemCount: times.length,
+                scrollDirection: Axis.horizontal,
+                ),
+              ),
             buildTitle('American CGV'),
+            Container(
+              margin: EdgeInsets.only(top: 8,bottom: 16),
+              height: size.height / 15,
+              child: ListView.builder(
+                itemBuilder: (context, index) => buildTime(size, index, timeStates_3),
+                itemCount: times.length,
+                scrollDirection: Axis.horizontal,
+                ),
+              ),
           ],
         ),
       ),
     );
   }
+
+  Container buildDate(Size size, String e) {
+    return Container(
+                    height: size.height / 8,
+                    width: size.width / 5,
+                    decoration: BoxDecoration(
+                      color: _getColor(dateStates[days.indexOf(e)]),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(e,style: TxtStyle.heading3,),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4),
+                          child: Text((20 + days.indexOf(e)).toString(),style: TxtStyle.heading3Medium),
+                        ),
+                      ],
+                    ),
+                  );
+  }
+
+  Container buildTime(Size size, int index, List<TicketStates> states) {
+    return Container(
+                margin: EdgeInsets.only(left: 24),
+                width: size.width / 4,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: _getColor(states[index]),
+                  borderRadius: BorderRadius.circular(14),
+                ),
+                child: Text(times[index],style: TxtStyle.heading3Medium,),
+              );
+  }
+  Color _getColor(TicketStates state) {
+      switch(state) {
+        case TicketStates.idle : {
+          return DarkTheme.darkBackground;
+        }
+        case TicketStates.active : {
+          return DarkTheme.blueMain;
+        }
+        case TicketStates.busy : {
+          return DarkTheme.greyBackground;
+        }
+      }
+      return DarkTheme.greyBackground;
+    }
 
   Container buildTitle(String content) {
     return Container(
